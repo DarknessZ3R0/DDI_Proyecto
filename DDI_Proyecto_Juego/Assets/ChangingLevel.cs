9673686c10699ac549fe5280a54eb296d8241550
Efren Mario Gonzalez Lopez <mgonzalez97@uabc.edu.mx>
2018-11-29 17:58:17 -0800

    scrips

---
+++ DDI_Proyecto_Juego/Assets/ChangingLevel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangingLevel : MonoBehaviour
+{
+    public bool enter = true;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (enter)
+        {
+            Debug.Log("entered");
+        }
+    }
+
+}
