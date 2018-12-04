3e33ec7c9ae38dad497282420af1d1e77ae770d3
Efren Mario Gonzalez Lopez <mgonzalez97@uabc.edu.mx>
2018-12-04 09:56:31 -0800

    Nuevos Scripts

---
+++ DDI_Proyecto_Juego/Assets/Script/doors.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doors : MonoBehaviour {
+	public int keys=0;
+	private bool isPlayerInside = false;
+	public GameObject puerta;
+
+	// Use this for initialization
+	void Start () {
+		puerta.SetActive(true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.E) && isPlayerInside)
+        	{
+           		 puerta.SetActive(false);
+            
+        	}
+	}
+	 private void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Se encontro jugador");
+            isPlayerInside = true;
+            
+        }
+        
+    }
+    	void OnTriggerExit(Collider other)
+	{
+		if (other.CompareTag("Player")) 
+		{
+			Debug.Log("Jugador salio");
+			isPlayerInside = false;
+		}
+	}
+}
---
+++ DDI_Proyecto_Juego/Assets/Script/onof.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onof : MonoBehaviour {
+	public GameObject lampara;
+	private bool on=true;
+
+	// Use this for initialization
+	void Start () {
+		lampara.SetActive(true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKeyDown(KeyCode.F))
+        	{	
+        		if(on==true){
+           		     lampara.SetActive(false);
+           		     on=false;
+        		}else{
+        			lampara.SetActive(true);
+        			on=true;
+        		}
+            
+        	}
+	}
+
+
+	/*private void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Se encontro jugador");
+            isPlayerInside = true;
+            
+        }
+    }
+   /*   	void OnTriggerExit(Collider other)
+	{
+		if (other.CompareTag("Player")) 
+		{
+			Debug.Log("Jugador salio");
+			isPlayerInside = false;
+		}
+	}*/
+}
+
