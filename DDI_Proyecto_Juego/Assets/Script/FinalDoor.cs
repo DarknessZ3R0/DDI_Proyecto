22af65b22c9b2ce60aa1be03f8f63b0ea9229a10
Efren Mario Gonzalez Lopez <mgonzalez97@uabc.edu.mx>
2018-12-05 12:30:52 -0800

    Scripts
    Mas modificaciones, se agrego script para fin de juego

---
+++ DDI_Proyecto_Juego/Assets/Script/FinalDoor.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalDoor : MonoBehaviour {
+	public GameObject door;
+	public GameObject panelLlaves;
+	public int Master_Key_Required=1;
+	private bool isPlayerInside = false;
+	
+    private int User_Keys = 0;
+	
+
+    // Use this for initialization
+    void Start () {
+		door.SetActive(true);
+		panelLlaves.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        User_Keys=GameObject.Find("ElJugador").GetComponent<Inventario>().MasterKey;
+        if (Input.GetKeyDown(KeyCode.E) && isPlayerInside && User_Keys==Master_Key_Required)
+        {
+           		 door.SetActive(false);
+
+        }
+      
+	}
+	 private void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.CompareTag("Player"))
+        {
+            isPlayerInside = true;
+			if(this.User_Keys != Master_Key_Required){
+				panelLlaves.SetActive(true);   
+			}
+			
+        }
+        
+    }
+    	void OnTriggerExit(Collider other)
+	{
+		if (other.CompareTag("Player")) 
+		{
+			isPlayerInside = false;
+			panelLlaves.SetActive(false);
+		}
+	}
+}
