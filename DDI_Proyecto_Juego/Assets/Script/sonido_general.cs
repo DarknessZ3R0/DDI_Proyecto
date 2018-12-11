90a40770823d1a4d078f35e8e2b3e07194e123f2
Unknown <kelly.inzunza@uabc.edu.mx>
2018-12-10 18:17:07 -0800

    se añadio sonido
    script

---
+++ DDI_Proyecto_Juego/Assets/Script/sonido_general.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sonido_general : MonoBehaviour {
+
+	 public GameObject objeto;
+    private AudioSource sonido_g;
+    
+
+    void Start () {
+       sonido_g = GetComponent<AudioSource>();//sonidos
+        
+	}
+	
+	void Update () {
+            if(objeto.activeInHierarchy)
+            {
+			    sonido_g.Stop();
+		    }else
+            {
+			    sonido_g.Play(); 	
+		    }
+        
+    }
+
+}
+
+
+
+
