db42e93e36115dc0334ee79d14971ace759c25c6
Christian Manuel Justo Romero <cjusto@uabc.edu.mx>
2018-12-13 00:03:29 -0800

    Musica
    Fin de juego musica y scripts

--- DDI_Proyecto_Juego/Assets/Script/EndGame.cs
+++ DDI_Proyecto_Juego/Assets/Script/EndGame.cs
@@ -3,14 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EndGame : MonoBehaviour {
-
+private bool isPlayerInside = false;
+public string nivel;
 	// Use this for initialization
-	void Start () {
+void Update () {
+        
 		
+        if ( isPlayerInside)
+        {
+           	LoadMapa();	 
+
+        }
+		
+		
+      
 	}
+
+	 private void LoadMapa()
+    {
+        Application.LoadLevel(nivel);
+    }
 	
-	// Update is called once per frame
-	void Update () {
-		
+	
+    
+	 private void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.CompareTag("Player"))
+        {		
+            isPlayerInside = true;
+        }
+        
+    }
+    private	void OnTriggerExit(Collider other)
+	{
+		if (other.CompareTag("Player")) 
+		{
+			isPlayerInside = false;
+			
+		}
 	}
 }
